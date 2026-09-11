8ac5df5c3ad5370b83b4046b99219be604a88917
Ujjwalkumar87 <ujjwalkumar8769@gmail.com>
2026-09-11 13:27:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (62.88%) - LeetSync

---
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] arr = new int[n][n];
+        int fr = 0;
+        int fc = 0;
+        int lr = n - 1;
+        int lc = n - 1;
+        int num = 1;
+        while(fr <= lr && fc <= lc){
+            for(int j = fc; j <= lc; j++)
+                arr[fr][j] = num++;
+                fr++; 
+                if(fr > lr || fc > lc) break;
+            
+            for(int i = fr; i <= lr; i++)
+                arr[i][lc] = num++;
+                lc--;
+                if(fr > lr || fc > lc) break;
+            
+            for(int j = lc; j >= fc; j--)
+                arr[lr][j] = num++;
+                lr--;
+                if(fr > lr || fc > lc) break;
+            
+            for(int i = lr; i >= fr; i--)
+                arr[i][fc] = num++;
+                fc++;
+                if(fr > lr || fc > lc) break;
+            
+        }
+        return arr;
+    }
+}
