aa19ec898274c415b45087d6a7cc3ad277506b11
Ujjwalkumar87 <ujjwalkumar8769@gmail.com>
2026-09-12 15:54:18 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (87.39%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumWealth(int[][] arr) {
+        int max = 0;
+        int[] ans = new int[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            int sum = 0;
+            for(int j = 0; j < arr[0].length; j++){
+                sum += arr[i][j];
+                ans[i] = sum;
+            }
+            max = Math.max(max , sum);
+        }
+        return max;
+    }
+}
