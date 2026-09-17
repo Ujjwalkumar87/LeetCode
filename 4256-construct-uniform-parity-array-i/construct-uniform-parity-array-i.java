3d5d4b940be52c915e7d88ca98717c18548f07d3
Ujjwalkumar87 <ujjwalkumar8769@gmail.com>
2026-09-17 22:40:13 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (14.14%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // int[] nums2 = new int[nums1.length];
+        // for(int i = 0; i < nums1.length; i++){
+        //     if(nums2[i] = nums1[i]) return true;
+        // }
+        // for(int j = 0; j < nums1.length; j++){
+        //     if(nums2[i] = nums1[i] - nums1[j] && j != i) return true;
+        // }
+        // return false;
+
+        // chat gpt 
+
+        
+        return true;
+    }
+}
