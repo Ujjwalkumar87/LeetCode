088fe79aa18648225a713d945c2106f42b3b69ff
Ujjwalkumar87 <ujjwalkumar8769@gmail.com>
2026-09-25 19:00:07 +0530

    Time: 12 ms (50.06%) | Memory: 48.3 MB (18.64%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,21 +1,25 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int low = 1 , high = 0;
-        for(int pile : piles) high = Math.max(high, pile);
-
+        for(int pile : piles) high = Math.max(high , pile);
+        
+        int ans = -1;
         while(low <= high){
-            int mid = low + (high - low)/2;
-            if(isValid(piles , h , mid)) high = mid - 1;
+            int mid = low + (high - low) / 2;
+            if(isValid(piles , mid) <= h){
+                high = mid - 1;
+                ans = mid;
+            }
             else low = mid + 1;
         }
-        return low;
+        return ans;
     }
-    public boolean isValid(int[] piles , int h , int speed){
-            long noh = 0;
-            for(int pile : piles){
-                if((pile % speed) == 0) noh += (pile / speed);
-                else noh += (pile / speed) + 1;
-            }
-            return noh <= h;
+    public long isValid(int[] piles , int speed){
+        long noh = 0;
+        for(int pile : piles){
+            if(pile % speed == 0) noh += pile / speed;
+            else noh += (pile / speed + 1);
         }
+        return noh;
+    }
 }
